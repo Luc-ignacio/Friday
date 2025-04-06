427d1dfdbd05b97092e3878128ec8f5727541586
Lucas Ignacio <114285355+Luc-ignacio@users.noreply.github.com>
2025-04-07 09:58:11 +1000

    Update sign in with Google

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { Button } from "../components/button.jsx";
 import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { Link } from "react-router-dom";
+import { doc, setDoc } from "firebase/firestore";
 
 function Login() {
   const [credentials, setCredentials] = useState({
@@ -48,6 +49,15 @@ function Login() {
   const SignUpWithGoogle = async () => {
     try {
       await signInWithPopup(auth, googleProvider);
+      if (user) {
+        [
+          await setDoc(doc(db, "users", user.uid), {
+            email: user.email,
+            firstName: auth?.currentUser.displayName,
+            lastName: "",
+          }),
+        ];
+      }
       window.location.href = "/profile";
       toast.success("User signed in successfully");
     } catch (error) {
